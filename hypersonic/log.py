c7a530b362176d6780ae9be4305cc4e1bcbd661e
Mario D'Andrea <goffredo2004@gmail.com>
2025-04-30 15:29:17 +0200

    add custom logger formatter

--- hypersonic/log.py
+++ hypersonic/log.py
@@ -1,11 +1,35 @@
 """Configure logging for the app"""
 
-from logging import getLogger, DEBUG, INFO, StreamHandler, Logger
+import logging
 from sys import stdout
+from typing import override
 
 
-def get_logger(module_name: str) -> Logger:
-    logger = getLogger(module_name)
-    logger.addHandler(StreamHandler(stream=stdout))
-    logger.setLevel(DEBUG if __debug__ else INFO)
+class ColoredFormatter(logging.Formatter):
+    RESET = '\033[0m'
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    MAGENTA = '\033[35m'
+    BOLD = '\033[1m'
+    FORMAT = '%(asctime)s %(filename)s:%(lineno)d %(levelname)s: %(message)s '
+    FORMATTERS = {
+        logging.DEBUG: logging.Formatter(MAGENTA + FORMAT + RESET),
+        logging.INFO: logging.Formatter(GREEN + FORMAT + RESET),
+        logging.WARNING: logging.Formatter(YELLOW + FORMAT + RESET),
+        logging.ERROR: logging.Formatter(RED + FORMAT + RESET),
+        logging.CRITICAL: logging.Formatter(BOLD + RED + FORMAT + RESET)
+    }
+
+    @override
+    def format(self, record):
+        return ColoredFormatter.FORMATTERS[record.levelno].format(record)
+
+
+def get_logger(module_name: str) -> logging.Logger:
+    logger = logging.getLogger(module_name)
+    stream_handler = logging.StreamHandler(stream=stdout)
+    stream_handler.setFormatter(ColoredFormatter())
+    logger.addHandler(stream_handler)
+    logger.setLevel(logging.DEBUG if __debug__ else logging.INFO)
     return logger
