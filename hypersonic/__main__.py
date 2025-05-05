01c6cefba4c977301566f272dfe579ae9169fd35
Mario D'Andrea <goffredo2004@gmail.com>
2025-05-05 10:31:11 +0200

    feat: basic agent

--- hypersonic/__main__.py
+++ hypersonic/__main__.py
@@ -8,13 +8,13 @@ from .entities import Bombardino, AspAgent
 
 
 def main():
-    model_update_rate = 10  # turns per second
+    model_update_rate = 2  # turns per second
     model_update_interval = 1 / model_update_rate
     model_accumulator = 0.0
     last_time = time()
 
     game = Game([
-        Bombardino(0, Game.START_POSITIONS[0], [os.path.join("encodings", "bombardino")], "Bombardino"),
+        Bombardino(0, Game.START_POSITIONS[0], [os.path.join("encodings", "bombardino.lp")], "Bombardino"),
         AspAgent(1, Game.START_POSITIONS[1], [os.path.join("encodings", "random.lp")], "Random")
     ])
     display = Display(game)
