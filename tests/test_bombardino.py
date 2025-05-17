a0a6834df8bc376fe81cf3a63d8ef9a5ed1458c5
Mario D'Andrea <goffredo2004@gmail.com>
2025-05-17 17:08:04 +0200

    feat: heed the enemy

--- tests/test_bombardino.py
+++ tests/test_bombardino.py
@@ -2,9 +2,10 @@
 
 import pytest
 import os
-from hypersonic.model import Game
-from hypersonic.entities import Bombardino, AspAgent
+from hypersonic.model import Game, CellType
+from hypersonic.entities import Bombardino, AspAgent, Bomb
 from hypersonic.layouts import LAYOUTS
+from .test_model import EMPTY_GRID
 
 
 @pytest.fixture(autouse=True)
@@ -53,3 +54,56 @@ def test_stays_put_when_it_reaches_the_optimum_but_has_not_the_bomb(game: Game):
     for _ in range(8):
         game.update()
         assert (game.agents[0].x, game.agents[0].y) == (1, 5), "doesn't move"
+
+
+def test_protects_itself_against_box_stealing(game: Game):
+    game.grid = [list(row) for row in EMPTY_GRID]
+    game.grid[5][5] = CellType.BOX.value
+
+    # The enemy stays at (12, 10) the whole time
+    # It takes 11 turns for the enemy to place a bomb that threatens the box
+
+    # The agent takes 10 + 8 turns to take out the box
+
+    for _ in range(10):
+        game.update()
+
+    #  assert abs(game.bombs[0].x - 5) + abs(game.bombs[0].y - 5) == 1, "bomb is placed next to the box"
+
+
+def test_chooses_suboptimal_when_enemy_is_closer_to_optimal_and_has_bomb(game: Game):
+    # If enemy stays still the agent will choose the actual best
+    game.agents[1] = Bombardino(1, Game.START_POSITIONS[1], [os.path.join("encodings", "bombardino.lp")])
+    game.agents[1].send_prelude(game.WIDTH, game.HEIGHT)
+
+    game.grid = [list(row) for row in EMPTY_GRID]
+    game.grid[5][2] = CellType.BOX.value # second choice
+    game.grid[5][10] = game.grid[7][8] = CellType.BOX.value
+    # (8, 5) is the best position
+
+    for _ in range(6):
+        game.update()
+    assert any(bomb for bomb in game.bombs if (bomb.x, bomb.y) in ((0, 5), (2, 3))), "places bomb where it supposed to"
+    # To-do: When we do stealing protection this must be changed
+
+    # Agent is closer but gets bomb after enemy
+    game.grid = [list(row) for row in EMPTY_GRID]
+
+
+def test_chooses_suboptimal_when_it_is_closer_but_enemy_has_smaller_bomb_cooldown(game: Game):
+    game.agents[1] = Bombardino(1, (12, 7), [os.path.join("encodings", "bombardino.lp")])
+    game.agents[1].send_prelude(game.WIDTH, game.HEIGHT)
+    game.agents[0].bombs_left = 0
+    game.bombs.append(Bomb(0, Game.WIDTH - 1, 0))
+    game.agents[0].x, game.agents[0].y = 6, 3
+
+    game.grid = [list(row) for row in EMPTY_GRID]
+    game.grid[5][2] = CellType.BOX.value
+    game.grid[2][11] = game.grid[4][9] = CellType.BOX.value
+    # (9, 2) is the best position
+    # Agent's distance is 2 turns, enemy's distance is 7 turns
+
+    for _ in range(Bomb.LIFETIME):
+        game.update()
+
+    assert any(bomb for bomb in game.bombs if (bomb.x, bomb.y) in ((4, 5), (2, 3))), "places bomb where it is supposed to"
